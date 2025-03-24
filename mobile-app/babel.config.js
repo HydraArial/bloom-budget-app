0804c32b09f1c48928e90bd344271269f8bfa280
Jay <jaybhatade7@gmail.com>
2025-03-24 20:33:33 +0530

    new app

--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,9 +1,10 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: ['nativewind/babel'],
-      plugins: ['expo-router/babel'],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  const plugins = [];
+
+  return {
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+
+    plugins,
   };
-  
+};
--- mobile-app/tailwind.config.js
+++ mobile-app/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+
+  presets: [require('nativewind/preset')],
   theme: {
     extend: {},
   },
